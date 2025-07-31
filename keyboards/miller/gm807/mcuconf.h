70ec233d5672b3e879ab3dff76929653381d59ac
naix07 <xian07@protonmail.com>
2025-08-01 00:36:08 +0800

    use updated driver

---
+++ keyboards/miller/gm807/mcuconf.h
@@ -0,0 +1,30 @@
+/* Copyright 2025 naix07
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#define SN32_SPI_USE_SPI0 TRUE
+
+#undef SN32_PWM_USE_CT16B1
+#define SN32_PWM_USE_CT16B1 TRUE
+
+#undef SN32_PWM_NO_RESET
+#define SN32_PWM_NO_RESET TRUE
+
+#define ITON_BT_ENABLE_ACK TRUE
+#define PAL_USE_CALLBACKS TRUE
+
+#include_next <mcuconf.h>
--- keyboards/miller/gm807/rules.mk
+++ keyboards/miller/gm807/rules.mk
@@ -2,15 +2,5 @@ BACKLIGHT_ENABLE  = no       # Enable keyboard backlight functionality
 AUDIO_ENABLE      = no       # Audio output
 
 WAIT_FOR_USB      = no
-CUSTOM_MATRIX     = yes
-ENCODER_ENABLE    = yes
 
 BLUETOOTH_ENABLE  = yes
-BLUETOOTH_DRIVER  = ITON_BT
-
-# Custom RGB matrix handling
-RGB_MATRIX_ENABLE = yes
-RGB_MATRIX_DRIVER = SN32F24xB
-
-# project specific files
-SRC = ../../../drivers/led/sn32/matrix_sn32f24xx.c
