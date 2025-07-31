d135de6826d7e825cc353ed503d63fc165c67ee5
naix07 <xian07@protonmail.com>
2025-08-01 00:32:02 +0800

    migrate to keyboard.json

--- keyboards/miller/gm807/rules.mk
+++ keyboards/miller/gm807/rules.mk
@@ -1,15 +1,3 @@
-# MCU name
-MCU = SN32F248BF
-
-# Build Options
-#   comment out to disable the options.
-#
-BOOTMAGIC_ENABLE  = yes      # Enable Bootmagic Lite
-MOUSEKEY_ENABLE   = no       # Mouse keys
-EXTRAKEY_ENABLE   = yes      # Audio control and System control
-CONSOLE_ENABLE    = no       # Console for debug
-COMMAND_ENABLE    = no       # Commands for debug and configuration
-NKRO_ENABLE       = yes      # Enable N-Key Rollover
 BACKLIGHT_ENABLE  = no       # Enable keyboard backlight functionality
 AUDIO_ENABLE      = no       # Audio output
 
